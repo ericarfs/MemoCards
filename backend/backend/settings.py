9bd5743a36f3cbb0e5335a773a936edaed49c3fb
Erica Ribeiro <ericaribeiro@usp.br>
2024-09-11 13:40:21 -0300

    update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tv($v7=z@r$7x2%b5_#fo#o#p$qh@gb_#pgtue*hsb!1d!_v#a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
