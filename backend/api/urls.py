3143dfda6f2ddb461aa5110bd09b49dcc32f4072
Erica Ribeiro <ericaribeiro@usp.br>
2024-09-13 21:16:43 -0300

    update

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path("cards/",views.CardListCreate.as_view(), name="card-list"),
     path("cards/<int:pk>/",views.CardDetail.as_view(), name="card-detail"),
-    #path("cards/update/<int:pk>/",views.CardUpdate.as_view(), name="update-card"),
 ]
