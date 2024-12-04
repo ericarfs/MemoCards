98722752b06b942f4d8844bd477512d8b00649d2
Erica Ribeiro <ericaribeiro@usp.br>
2024-12-03 21:36:17 -0300

    update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl =
-  "https://b5644af6-37fb-45a3-9d6a-ef42087e1027-dev.e1-us-east-azure.choreoapis.dev/memocards/backend/v1.0";
+const apiUrl = "/choreo-apis/memocards/backend/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
